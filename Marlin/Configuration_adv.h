640a8b3297a844a405cdd8347d05ba02aaba05d7
Luis Martins <chateadorn@gmail.com>
2023-01-24 21:39:14 -0300

    carregando config já salva dentro do ESP32 ao inicializar as cores

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1263,7 +1263,7 @@
       #define LED_USER_PRESET_BLUE         0  // User defined BLUE value
       #define LED_USER_PRESET_WHITE      255  // User defined WHITE value
       #define LED_USER_PRESET_BRIGHTNESS 255  // User defined intensity
-      //#define LED_USER_PRESET_STARTUP       // Have the printer display the user preset color on startup
+      #define LED_USER_PRESET_STARTUP       // Have the printer display the user preset color on startup
     #endif
     #if ENABLED(NEO2_COLOR_PRESETS)
       #define NEO2_USER_PRESET_RED        255  // User defined RED value
